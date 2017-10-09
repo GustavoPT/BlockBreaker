96b97014025589304ded0d00ed03ee7ba2ca874d
Gustavo <gustxw@gmail.com>
2017-10-09 15:21:41 -0400

    first

---
+++ BlockBreaker/BlockBreaker/Assets/Scripts/Losecollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Losecollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D trigger){
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		Brick.breakableCount = 0;
+		levelManager.LoadLevel("Loose Screen");
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		print("Collision");
+	}
+	
+}
---
+++ GlitchGarden/Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+
+	public AudioClip[] levelMusicChangeArray;
+	
+	private AudioSource audioSource;
+	// Use this for initialization
+	
+	
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+		Debug.Log("Don't destroy on load: " + name);
+	
+	}
+	
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	
+	}
+	
+	void OnLevelwasLoaded(int level){
+		Debug.Log("Playing Clip"+levelMusicChangeArray[level]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
